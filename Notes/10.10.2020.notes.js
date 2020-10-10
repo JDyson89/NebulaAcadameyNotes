fcdcb8430fbd294e17e00f6dfe3becf7d2bb2724
Jasmine Dyson <J.dyson89@yahoo.com>
2020-10-10 14:00:31 -0400

    added string methods to notes

---
+++ Notes/10.10.2020.notes.js
@@ -0,0 +1,55 @@
+let quote = "\"whats up\""
+console.log(quote)
+
+let Daja = "d\'aja"
+console.log(Daja)
+
+
+let example1 = "nebula";
+console.log(example1);
+console.log(example1[0]);
+console.log(example1[1]);
+console.log(example1[2]);
+console.log(example1[3]);
+console.log(example1[4]);
+console.log(example1[5]);
+
+
+let jasmine = "Jasmine Dyson"
+console.log(jasmine);
+let dyson = "Jasmine\nDyson"
+console.log(dyson);
+
+let myName = "Jasmine Dyson";
+console.log(`${myName[0]}${myName[8]}`);
+console.log(myName[0]);
+console.log(myName[8]);
+
+let firstLetter = myName[0];
+let secondLetter = myName[8];
+console.log(firstLetter + secondLetter);
+
+console.log(myName[myName.length-1]);
+
+
+let start = "Jasmine Dyson"
+console.log(start.charAt(0));
+console.log(start.charAt(2));
+console.log(start.charAt(3));
+console.log(start.charAt(4));
+console.log(start.charAt(5));
+console.log(start.charAt(6));
+console.log(start.charAt(7));
+console.log(start.charAt(8));
+console.log(start.charAt(9));
+console.log(start.charAt(10));
+console.log(start.charAt(11));
+console.log(start.charAt(12));
+
+console.log(start);
+console.log(start.toUpperCase());
+console.log(start.toLowerCase());
+
+console.log(start.replace(/s/g, "-"));
+
+
