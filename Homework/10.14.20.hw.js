d414d55dc008bf2a52219fb748c876daae03438b
Jasmine Dyson <J.dyson89@yahoo.com>
2020-10-17 10:01:17 -0400

    homework for 10.14.20

---
+++ Homework/10.14.20.hw.js
@@ -0,0 +1,21 @@
+// let master = "we are software engineers at nebula academy"
+// console.log(master.indexOf('software'));
+// console.log(master.indexOf('s'));
+// console.log(master.indexOf('web'));
+// console.log(master.includes('z'));
+// console.log(master.includes('engineer'));
+// console.log(master.split(" "));
+
+
+
+
+let mile = "we are learning new string methods";
+console.log(mile.length);
+console.log(mile[33]);
+// console.log(mile.indexOf[5]);
+console.log(mile.replace(/a/g, "-"));
+console.log(mile.toUpperCase());
+console.log(mile.toLowerCase());
+
+
+let iceCream = "My favorite ice cream flavor is mint chocolate chip"
