f3127ef3b29837a9acc29492f25e84ab5500ac7c
Jasmine Dyson <J.dyson89@yahoo.com>
2020-10-23 19:30:38 -0400

    homework 10.21.20 assignment added

--- Homework/10.14.20.hw.js
+++ Homework/10.14.20.hw.js
@@ -5,17 +5,16 @@
 // console.log(master.includes('z'));
 // console.log(master.includes('engineer'));
 // console.log(master.split(" "));
+// console.log(master.slice(7, 15));
+// console.log(master.slice(-7))
 
-
-
-
-let mile = "we are learning new string methods";
-console.log(mile.length);
-console.log(mile[33]);
+// let mile = "we are learning new string methods";
+// console.log(mile.length);
 // console.log(mile.indexOf[5]);
-console.log(mile.replace(/a/g, "-"));
-console.log(mile.toUpperCase());
-console.log(mile.toLowerCase());
+// console.log(mile.toUpperCase());
+// console.log(mile.toLowerCase());
 
 
-let iceCream = "My favorite ice cream flavor is mint chocolate chip"
+// let iceCream = "mint chocolate chip";
+// console.log(`My favorite ice cream flavor is ${iceCream}`); //backtick
+// console.log("My favorite ice cream flavor is " + iceCream);
