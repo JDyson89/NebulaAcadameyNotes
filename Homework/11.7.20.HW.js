e8c4679f1a879118c0ce953e6379def7d164da69
Jasmine Dyson <J.dyson89@yahoo.com>
2020-11-13 21:52:33 -0500

    11.9.20 hw

--- Homework/11.7.20.HW.js
+++ Homework/11.7.20.HW.js
@@ -12,15 +12,16 @@
 
 // Create a function that takes in a number as a parameter.
 // This function will log from 1- any number passed in this function as an argument
-// After all numbers are successfully logged, return the string, 'Done!'
+// // After all numbers are successfully logged, return the string, 'Done!'
 
-// function oneToNine (){
-//     for (let i = 1; i <= 9; i++){
+// function oneToNine (numbers){
+//     for (let i = 1; i <= numbers; i++){
 //         console.log(i)
 //     }
 //     return 'Done!'
 // }
-// console.log(oneToNine())
+// console.log(oneToNine(9))
+// console.log(oneToNine(5))
 
 
 // Create a function that takes in an array as a parameter.
@@ -42,20 +43,20 @@
 // This function will loop through the string and log the indices of each character in the string.
 // After the indices are logged, return the string, 'Done!'
 
-// let takeInString = 'Thanksgiving is soon'
+// let takeInString = 'Thanks'
 
 // function stringLoop (){
 //     for (let i = 0; i <= takeInString.length - 1; i++){
-//         console.log([i])
+//         console.log(i)
         
 //     }
 //     return 'Done!'
 // }
-// console.log(stringLoop(takeInString.charAt))
+// console.log(stringLoop(takeInString))
 
 
-// Create a function that only logs the even numbers from 1-10
-// After the even numbers are logged, return the string, 'Done!'
+// // Create a function that only logs the even numbers from 1-10
+// // After the even numbers are logged, return the string, 'Done!'
 
 // let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] 
 
@@ -66,3 +67,16 @@
 //         return 'Done!'   
 // }
 // console.log(evenNums(numbers))
+
+// Another Way
+
+function evenNum(){
+    for (let i = 1; i <= 10; i ++){
+        //any number divided by 2 has to have no remainder
+        if (i % 2 === 0){
+            console.log(i)
+        }
+    }
+    return 'Done!'
+}
+console.log(evenNum())
